00f7bab8ff080d54681c6f1094e5b3d985100dde
Chase McCoy <chsmc@stripe.com>
2021-09-18 11:34:53 -0500

    ~

--- src/utils/compile-mdx.js
+++ src/utils/compile-mdx.js
@@ -21,7 +21,7 @@ async function compileMdx(slug, githubFiles) {
   })
 
   const imageTransformer = (tree) => {
-		// ❗ Commenting out the next 3 lines fixes the issue
+    // ❗ Commenting out the next 3 lines fixes the issue
     visit(tree, 'image', (node, index, parent) => {
       console.log('This breaks?')
     })
